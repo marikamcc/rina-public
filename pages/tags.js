e159068c53c7a8cada0c900cc2f950ab6f7997a2
marikamcc <69483439+marikamcc@users.noreply.github.com>
2023-01-12 13:19:15 -0800

    Tag page sorts by _date_ instead of by postid

--- pages/tags.js
+++ pages/tags.js
@@ -8,13 +8,36 @@ import prisma from '../lib/prisma';
 const pageTitle = "List of tags"
 
 export async function getStaticProps() {
-    const allTags = await prisma.tag.findMany({
+    // const allTags = await prisma.tag.findMany({
+    //     select: {
+    //         tagmap: {
+    //             select: { postid: true },
+    //             orderBy: { postid: 'desc' }, take: 1,
+    //         },
+    //         name: true,
+    //     },
+    // },
+
+    // )
+
+    const allTags2 = await prisma.tag.findMany({
         select: {
+            name:true,
             tagmap: {
-                select: { postid: true },
-                orderBy: { postid: 'desc' }, take: 1,
-            },
-            name: true,
+                select: {
+                    posts: {
+                        select: {
+                            date: true,
+                        }
+                    }
+                },
+                orderBy: {
+                    posts: {
+                        date: 'desc'
+                    }
+                },
+                take: 1,
+            }
         },
     },
 
@@ -24,14 +47,18 @@ export async function getStaticProps() {
     // I could probably do the recollect/sort in relation to the above query,
     // but I would need to think about it harder.
     const cleanerTags = [];
-    allTags.map((tag) => {
+    allTags2.map((tag) => {
         if (tag.tagmap[0]) {
-            cleanerTags.push({ recentid: tag.tagmap[0].postid, name: tag.name })
+            cleanerTags.push({ recentid: tag.tagmap[0].posts.date, name: tag.name })
         }
     })
     // Sort by recently used
     cleanerTags.sort((a, b) => {
-        return b.recentid - a.recentid;
+        const date1 = new Date(a.recentid);
+        const date2 = new Date(b.recentid);
+    
+    return date2 - date1;
+        //return b.recentid - a.recentid;
     });
 
     return {
@@ -42,6 +69,7 @@ export async function getStaticProps() {
 }
 
 export default function Tags({ cleanerTags }) {
+
     return (
         <Layout>
             <Head>
