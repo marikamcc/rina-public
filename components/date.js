c217e0e909ca948c74281c102d6dbbeed4cae3ae
marikamcc <69483439+marikamcc@users.noreply.github.com>
2023-01-24 12:09:11 -0800

    added timeago.js for timestamps

--- components/date.js
+++ components/date.js
@@ -1,4 +1,6 @@
-import { parseISO, format } from 'date-fns';
+// import { parseISO, format } from 'date-fns';
+import { parseISO } from 'date-fns';
+import { format } from 'timeago.js';
 
 export default function Date({ dateString }) {
   const date = parseISO(dateString);
